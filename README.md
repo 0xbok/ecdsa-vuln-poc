7da1fd61b327ce2c18c777c8fd0327506e899ed5
0xbok <1689531+0xbok@users.noreply.github.com>
2022-09-22 17:40:10 +0530

    add steps

--- README.md
+++ README.md
@@ -7,8 +7,11 @@ From the article:
 Go to `Counter.t.sol` to see how you can change the signature for Ethereum's Secp256k1 curve.
 Once someone signs a message and produces a signature, just using the signature, you can create another signature that is perfectly valid (that is `ecrecover` will tell you the original signer signed it).
 
-Just run `forge test -vv` and see both signatures producing the same recovered address.
+## Steps to run
+- `forge install` ([Install foundry](https://book.getfoundry.sh/getting-started/installation) if you haven't).
+- `forge test -vv` and two same addresses should be printed on screen.
 
+## Be safe
 You can be safe from these footguns by using OpenZeppelin's ECDSA library instead of the vanilla `ecrecover` operation.
 
 Please note that just using this library won't protect you from all signature vulnerabilities.
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-contract CounterTest is Test {
+contract SigVerifierTest is Test {
     // group order value of Secp256k1 curve
     // https://github.com/bmancini55/bitcoin-ecc/blob/df048235cdc89c4dd9cd253310637061ed17f5e8/lib/Secp256k1.ts#L7
     bytes32 constant public groupOrder = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141;
@@ -11,7 +11,7 @@ contract CounterTest is Test {
     }
 
     function testMalleable() public {
-        // r, s value from https://blog.openzeppelin.com/signing-and-validating-ethereum-signatures
+        // r, s, hash value from https://blog.openzeppelin.com/signing-and-validating-ethereum-signatures
         bytes32 r = 0x1556a70d76cc452ae54e83bb167a9041f0d062d000fa0dcb42593f77c544f647;
         bytes32 s = 0x1643d14dbd6a6edc658f4b16699a585181a08dba4f6d16a9273e0e2cbed622da;
 
